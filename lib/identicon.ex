a2d0f21177275e5dba6e947e54300eb99077ce97
Marc GRIS <62659262+m-gris@users.noreply.github.com>
2023-11-28 00:02:40 +0100

    add main() & hash_input()

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,5 +3,22 @@ defmodule Identicon do
   Documentation for `Identicon`.
   """
 
+  def main(input) do
+    input
+    |> hash_input
+  end
+  
+  @doc """
+  
+  ## Examples 
+
+    iex> Identicon.hash_input("Elixir")
+    [161, 46, 176, 98, 236, 169, 209, 230, 198, 159, 207, 139, 96, 55, 135, 195]
+
+  """
+  def hash_input(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+  end
 
 end
