f8b03c9c0dc9b2bd3ad40e4faff62eba32ed015b
Marc GRIS <62659262+m-gris@users.noreply.github.com>
2023-11-28 00:02:54 +0100

    simple test for hash_input()

--- test/identicon_test.exs
+++ test/identicon_test.exs
@@ -2,5 +2,11 @@ defmodule IdenticonTest do
   use ExUnit.Case
   doctest Identicon
 
+  test "hash_input is deterministic" do
+    input = "Hello"
+    first_hash = Identicon.hash_input(input) 
+    second_hash =  Identicon.hash_input(input)
+    assert first_hash == second_hash
+  end
 
 end
