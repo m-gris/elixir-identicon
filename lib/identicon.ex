7735de85b61b248b2672433251d608b3cb2be003
Marc GRIS <62659262+m-gris@users.noreply.github.com>
2023-11-29 08:09:40 +0100

    fix doctests

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -12,13 +12,37 @@ defmodule Identicon do
   
   ## Examples 
 
-    iex> Identicon.hash_input("Elixir")
+    iex> Map.get(Identicon.hash_input("Elixir"), :hex)
     [161, 46, 176, 98, 236, 169, 209, 230, 198, 159, 207, 139, 96, 55, 135, 195]
+    
 
   """
   def hash_input(input) do
-    :crypto.hash(:md5, input)
+    hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
+    %Identicon.Image{hex: hex}
+  end
+
+
+
+
+  
+  @doc """ 
+  
+  ## Examples 
+
+    iex> hash = Identicon.hash_input("Elixir")
+    iex> Map.get(Identicon.get_color(hash), :color)
+    {161, 46, 176}
+
+  """
+  def get_color(image) do
+    %Identicon.Image{hex: [r, g, b | _tail] } = image
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+
+  
   end
 
 end
